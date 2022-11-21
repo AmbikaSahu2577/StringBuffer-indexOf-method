83441b568318d1560c426b3fdee6160692dde546
Ambika Sahu <90752801+AmbikaSahu2577@users.noreply.github.com>
2022-11-21 14:46:49 +0530

    Add files via upload

---
+++ String_Buffer_indexOf_Method.java
@@ -0,0 +1,8 @@
+class String_Buffer_indexOf_Method
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer sb = new StringBuffer("java progrraming");
+		System.out.println(sb.indexOf("v"));
+	}
+}
